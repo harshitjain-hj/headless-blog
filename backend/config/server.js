d5887df491725c7d5a2bd07650accf6bdf900f1c
edoc99 <harshitjainf3@gmail.com>
2021-07-27 21:19:19 +0530

    add: added initial required collections

---
+++ backend/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '8b0ceb43604f914fb72fc5360fe0764f'),
+    },
+  },
+});
---
+++ backend/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '315f51cd-827c-48df-8216-f87b3b6c66d8'
+};
